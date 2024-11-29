69e89c0925fc89343115a99a790e21fc302a6302
a-dadychanka <artsiom.dadychanka@oqtacore.com>
2024-11-29 19:49:52 +0100

    Update widget config

--- src/features/WormholeWidget/config.ts
+++ src/features/WormholeWidget/config.ts
@@ -6,33 +6,25 @@ export const WORMHOLE_WIDGET_CONFIG: WormholeConnectConfig = {
     LINGO: {
       key: 'LINGO',
       symbol: 'LINGO',
-      // TODO: Replace with Base once address is available
-      nativeChain: 'Solana',
+      nativeChain: 'BaseSepolia',
+      // wrappedAsset: APP_CONFIG.LINGO_SOL_ADDRESS,
       tokenId: {
-        // TODO: Replace with Base once address is available
-        chain: 'Solana',
-        // TODO: Replace with LINGO_BASE_ADDRESS
-        address: APP_CONFIG.LINGO_SOL_ADDRESS,
+        chain: 'BaseSepolia',
+        address: APP_CONFIG.LINGO_BASE_ADDRESS,
       },
       icon: '/$LINGO.svg',
       // TODO: Replace with actual value for LINGO coin
-      coinGeckoId: 'solana',
+      coinGeckoId: '',
       decimals: 9,
     },
   },
-  // TODO: Uncomment once address of smart-contract on Base is available
-  // wrappedTokens: {
-  //   LINGO: {
-  //     Solana: APP_CONFIG.LINGO_SOL_ADDRESS,
-  //   },
-  // },
+  wrappedTokens: {
+    LINGO: {
+      Solana: APP_CONFIG.LINGO_SOL_ADDRESS,
+    },
+  },
   network: APP_CONFIG.WORMHOLE_NETWORK,
-  chains: [
-    'Solana',
-    'Base',
-    // Base testnet
-    'BaseSepolia',
-  ],
+  chains: ['BaseSepolia', 'Base', 'Solana'],
   rpcs: {
     Solana: APP_CONFIG.SOLANA_PROVIDER,
     Base: APP_CONFIG.BASE_PROVIDER,
