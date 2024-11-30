5307e5772b71a0b3c5ee567c75207242526c04c5
a-dadychanka <artsiom.dadychanka@oqtacore.com>
2024-11-30 11:39:10 +0100

    Change decimals

--- src/features/WormholeWidget/config.ts
+++ src/features/WormholeWidget/config.ts
@@ -7,7 +7,7 @@ export const WORMHOLE_WIDGET_CONFIG: WormholeConnectConfig = {
       key: 'LINGO',
       symbol: 'LINGO',
       nativeChain: 'BaseSepolia',
-      // wrappedAsset: APP_CONFIG.LINGO_SOL_ADDRESS,
+      // wrappedAsset: APP_CONFIG.LINGO_BASE_ADDRESS,
       tokenId: {
         chain: 'BaseSepolia',
         address: APP_CONFIG.LINGO_BASE_ADDRESS,
@@ -15,9 +15,40 @@ export const WORMHOLE_WIDGET_CONFIG: WormholeConnectConfig = {
       icon: '/$LINGO.svg',
       // TODO: Replace with actual value for LINGO coin
       coinGeckoId: '',
-      decimals: 9,
+      decimals: 18,
     },
   },
+  // routes: [
+  //   ...DEFAULT_ROUTES,
+  //   ...nttRoutes({
+  //     tokens: {
+  //       LINGO: [
+  //         {
+  //           chain: 'BaseSepolia',
+  //           manager: '0x6aD1422533Ff4BC90706511374F3b591dd9A7BaE',
+  //           token: '0x1b0981608F8D5Fb22fa38Bd445a877938cBf90FA',
+  //           transceiver: [
+  //             {
+  //               address: '0xeA8D34fa9147863e486d2d07AB92b8218CF58C0E',
+  //               type: 'wormhole',
+  //             },
+  //           ],
+  //         },
+  //         {
+  //           chain: 'Solana',
+  //           manager: 'keyeyYo2ztQTeeMYkbMG661rSH6xWtSaooAGPRcnrxZ',
+  //           token: 'supq9XjVD9nJHQ5vaTpxA1yXeSUvgWQXzaSkJr6THWv',
+  //           transceiver: [
+  //             {
+  //               address: '0x76516c0b966B4D4cFEFB107755562b16427dAE52',
+  //               type: 'wormhole',
+  //             },
+  //           ],
+  //         },
+  //       ],
+  //     },
+  //   }),
+  // ],
   wrappedTokens: {
     LINGO: {
       Solana: APP_CONFIG.LINGO_SOL_ADDRESS,
