68d3f1197d2a96d365f6f93e638d0547d95b5191
a-dadychanka <artsiom.dadychanka@oqtacore.com>
2024-12-01 19:45:55 +0100

    Update NTT routes

--- src/features/WormholeWidget/config.ts
+++ src/features/WormholeWidget/config.ts
@@ -1,4 +1,7 @@
-import { type WormholeConnectConfig } from '@wormhole-foundation/wormhole-connect';
+import {
+  type WormholeConnectConfig,
+  nttRoutes,
+} from '@wormhole-foundation/wormhole-connect';
 import { APP_CONFIG } from '@/settings';
 
 export const WORMHOLE_WIDGET_CONFIG: WormholeConnectConfig = {
@@ -6,8 +9,8 @@ export const WORMHOLE_WIDGET_CONFIG: WormholeConnectConfig = {
     LINGO: {
       key: 'LINGO',
       symbol: 'LINGO',
+      // wrappedAsset: APP_CONFIG.LINGO_SOL_ADDRESS,
       nativeChain: 'BaseSepolia',
-      // wrappedAsset: APP_CONFIG.LINGO_BASE_ADDRESS,
       tokenId: {
         chain: 'BaseSepolia',
         address: APP_CONFIG.LINGO_BASE_ADDRESS,
@@ -18,44 +21,43 @@ export const WORMHOLE_WIDGET_CONFIG: WormholeConnectConfig = {
       decimals: 18,
     },
   },
-  // routes: [
-  //   ...DEFAULT_ROUTES,
-  //   ...nttRoutes({
-  //     tokens: {
-  //       LINGO: [
-  //         {
-  //           chain: 'BaseSepolia',
-  //           manager: '0x6aD1422533Ff4BC90706511374F3b591dd9A7BaE',
-  //           token: '0x1b0981608F8D5Fb22fa38Bd445a877938cBf90FA',
-  //           transceiver: [
-  //             {
-  //               address: '0xeA8D34fa9147863e486d2d07AB92b8218CF58C0E',
-  //               type: 'wormhole',
-  //             },
-  //           ],
-  //         },
-  //         {
-  //           chain: 'Solana',
-  //           manager: 'keyeyYo2ztQTeeMYkbMG661rSH6xWtSaooAGPRcnrxZ',
-  //           token: 'supq9XjVD9nJHQ5vaTpxA1yXeSUvgWQXzaSkJr6THWv',
-  //           transceiver: [
-  //             {
-  //               address: '0x76516c0b966B4D4cFEFB107755562b16427dAE52',
-  //               type: 'wormhole',
-  //             },
-  //           ],
-  //         },
-  //       ],
-  //     },
-  //   }),
-  // ],
+  routes: [
+    ...nttRoutes({
+      tokens: {
+        LINGO: [
+          {
+            chain: 'BaseSepolia',
+            manager: '0x6aD1422533Ff4BC90706511374F3b591dd9A7BaE',
+            token: '0x1b0981608F8D5Fb22fa38Bd445a877938cBf90FA',
+            transceiver: [
+              {
+                address: '0xeA8D34fa9147863e486d2d07AB92b8218CF58C0E',
+                type: 'wormhole',
+              },
+            ],
+          },
+          {
+            chain: 'Solana',
+            manager: 'keyeyYo2ztQTeeMYkbMG661rSH6xWtSaooAGPRcnrxZ',
+            token: 'supq9XjVD9nJHQ5vaTpxA1yXeSUvgWQXzaSkJr6THWv',
+            transceiver: [
+              {
+                address: '0x76516c0b966B4D4cFEFB107755562b16427dAE52',
+                type: 'wormhole',
+              },
+            ],
+          },
+        ],
+      },
+    }),
+  ],
   wrappedTokens: {
     LINGO: {
       Solana: APP_CONFIG.LINGO_SOL_ADDRESS,
     },
   },
   network: APP_CONFIG.WORMHOLE_NETWORK,
-  chains: ['BaseSepolia', 'Base', 'Solana'],
+  chains: ['BaseSepolia', 'Solana'],
   rpcs: {
     Solana: APP_CONFIG.SOLANA_PROVIDER,
     Base: APP_CONFIG.BASE_PROVIDER,
