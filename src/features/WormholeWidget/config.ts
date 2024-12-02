148ed510b8c688d60ef7e7bd13fab1d55c86736f
Yasin Onur Yücel <yoy1992@gmail.com>
2024-12-02 09:18:26 +0100

    feat(config): add LINGO token configuration and routes for BaseSepolia and Solana
    - Added LINGO token to `tokensConfig` with BaseSepolia as the native chain.
    - Updated `wrappedTokens` with Solana address for LINGO.
    - Integrated RPC endpoints for Solana, Base, and BaseSepolia.

--- src/features/WormholeWidget/config.ts
+++ src/features/WormholeWidget/config.ts
@@ -1,7 +1,4 @@
-import {
-  type WormholeConnectConfig,
-  nttRoutes,
-} from '@wormhole-foundation/wormhole-connect';
+import { DEFAULT_ROUTES, nttRoutes, type WormholeConnectConfig } from '@wormhole-foundation/wormhole-connect';
 import { APP_CONFIG } from '@/settings';
 
 export const WORMHOLE_WIDGET_CONFIG: WormholeConnectConfig = {
@@ -9,55 +6,55 @@ export const WORMHOLE_WIDGET_CONFIG: WormholeConnectConfig = {
     LINGO: {
       key: 'LINGO',
       symbol: 'LINGO',
-      // wrappedAsset: APP_CONFIG.LINGO_SOL_ADDRESS,
       nativeChain: 'BaseSepolia',
+      wrappedAsset: APP_CONFIG.LINGO_BASE_ADDRESS,
       tokenId: {
         chain: 'BaseSepolia',
         address: APP_CONFIG.LINGO_BASE_ADDRESS,
       },
       icon: '/$LINGO.svg',
-      // TODO: Replace with actual value for LINGO coin
       coinGeckoId: '',
       decimals: 18,
     },
   },
-  routes: [
-    ...nttRoutes({
-      tokens: {
-        LINGO: [
-          {
-            chain: 'BaseSepolia',
-            manager: '0x6aD1422533Ff4BC90706511374F3b591dd9A7BaE',
-            token: '0x1b0981608F8D5Fb22fa38Bd445a877938cBf90FA',
-            transceiver: [
-              {
-                address: '0xeA8D34fa9147863e486d2d07AB92b8218CF58C0E',
-                type: 'wormhole',
-              },
-            ],
-          },
-          {
-            chain: 'Solana',
-            manager: 'keyeyYo2ztQTeeMYkbMG661rSH6xWtSaooAGPRcnrxZ',
-            token: 'supq9XjVD9nJHQ5vaTpxA1yXeSUvgWQXzaSkJr6THWv',
-            transceiver: [
-              {
-                address: '0x76516c0b966B4D4cFEFB107755562b16427dAE52',
-                type: 'wormhole',
-              },
-            ],
-          },
-        ],
-      },
-    }),
-  ],
+   routes: [
+     ...DEFAULT_ROUTES,
+     ...nttRoutes({
+       tokens: {
+         LINGO: [
+           {
+             chain: 'BaseSepolia',
+             manager: '0x6aD1422533Ff4BC90706511374F3b591dd9A7BaE',
+             token: '0x1b0981608F8D5Fb22fa38Bd445a877938cBf90FA',
+             transceiver: [
+               {
+                 address: '0xeA8D34fa9147863e486d2d07AB92b8218CF58C0E',
+                 type: 'wormhole',
+               },
+             ],
+           },
+           {
+             chain: 'Solana',
+             manager: 'keyeyYo2ztQTeeMYkbMG661rSH6xWtSaooAGPRcnrxZ',
+             token: APP_CONFIG.LINGO_SOL_ADDRESS,
+             transceiver: [
+               {
+                 address: '0x76516c0b966B4D4cFEFB107755562b16427dAE52',
+                 type: 'wormhole',
+               },
+             ],
+           },
+         ],
+       },
+     }),
+   ],
   wrappedTokens: {
     LINGO: {
       Solana: APP_CONFIG.LINGO_SOL_ADDRESS,
     },
   },
   network: APP_CONFIG.WORMHOLE_NETWORK,
-  chains: ['BaseSepolia', 'Solana'],
+  chains: ['BaseSepolia', 'Base', 'Solana'],
   rpcs: {
     Solana: APP_CONFIG.SOLANA_PROVIDER,
     Base: APP_CONFIG.BASE_PROVIDER,
